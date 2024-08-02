e29b41dbaee65ad290355dfd650638336210996a
applepie321 <112625800+applepie321@users.noreply.github.com>
2024-08-02 21:49:12 +0900

    Chapter.07 client

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::io::{Read, Write};
 use std::net::{Ipv4Addr, TcpStream};
 use std::process;
@@ -11,44 +12,42 @@ fn msg(message: &str) {
 fn die(message: &str) {
     let err = std::io::Error::last_os_error();
     eprintln!("[{}] {}", err.raw_os_error().unwrap_or(0), message);
-    process::abort()
+    process::abort();
 }
 
 fn read_full(stream: &mut TcpStream, buf: &mut [u8]) -> std::io::Result<()> {
-    let mut bytes_read = 0;
-    while bytes_read < buf.len() {
-        match stream.read(&mut buf[bytes_read..]) {
-            Ok(0) => {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::UnexpectedEof,
-                    "EOF",
-                ))
-            }
-            Ok(n) => bytes_read += n,
-            Err(e) => return Err(e),
-        }
-    }
-    Ok(())
+    stream.read_exact(buf)
 }
 
 fn write_all(stream: &mut TcpStream, buf: &[u8]) -> std::io::Result<()> {
     stream.write_all(buf)
 }
 
-fn query(stream: &mut TcpStream, text: &str) -> std::io::Result<()> {
-    let len = text.len() as u32;
-    if len > K_MAX_MSG as u32 {
+fn send_req(stream: &mut TcpStream, cmd: &[String]) -> std::io::Result<()> {
+    let mut len = 4;
+    for s in cmd {
+        len += 4 + s.len();
+    }
+    if len > K_MAX_MSG {
         return Err(std::io::Error::new(
             std::io::ErrorKind::InvalidInput,
             "Message too long",
         ));
     }
 
-    let mut wbuf = vec![0u8; 4 + text.len()];
-    wbuf[0..4].copy_from_slice(&len.to_le_bytes());
-    wbuf[4..].copy_from_slice(text.as_bytes());
-    write_all(stream, &wbuf)?;
+    let mut wbuf = Vec::with_capacity(4 + K_MAX_MSG);
+    wbuf.extend_from_slice(&(len as u32).to_le_bytes());
+    wbuf.extend_from_slice(&(cmd.len() as u32).to_le_bytes());
 
+    for s in cmd {
+        wbuf.extend_from_slice(&(s.len() as u32).to_le_bytes());
+        wbuf.extend_from_slice(s.as_bytes());
+    }
+
+    write_all(stream, &wbuf)
+}
+
+fn read_res(stream: &mut TcpStream) -> std::io::Result<()> {
     let mut rbuf = vec![0u8; 4 + K_MAX_MSG + 1];
     read_full(stream, &mut rbuf[0..4])?;
 
@@ -62,18 +61,30 @@ fn query(stream: &mut TcpStream, text: &str) -> std::io::Result<()> {
 
     read_full(stream, &mut rbuf[4..4 + len])?;
 
-    let response = String::from_utf8_lossy(&rbuf[4..4 + len]);
-    println!("server says: {}", response);
+    if len < 4 {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "Bad response",
+        ));
+    }
+
+    let rescode = u32::from_le_bytes(rbuf[4..8].try_into().unwrap());
+    println!(
+        "server says: [{}] {}",
+        rescode,
+        String::from_utf8_lossy(&rbuf[8..4 + len])
+    );
+
     Ok(())
 }
 
 pub fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect((Ipv4Addr::LOCALHOST, 1234))?;
 
-    // multiple requests
-    query(&mut stream, "hello1")?;
-    query(&mut stream, "hello2")?;
-    query(&mut stream, "hello3")?;
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    send_req(&mut stream, &args)?;
+    read_res(&mut stream)?;
 
     Ok(())
 }
